c7967ab9a1086e42cb2a2985997542d81579d203
Rasmus Fonseca <fonseca.rasmus@gmail.com>
2015-08-25 06:05:42 +0200

    Comments in PatternUnit

--- src/pu/pattern_unit.h
+++ src/pu/pattern_unit.h
@@ -6,13 +6,25 @@
 #include "../match.h"
 #include "../Modifiers.h"
 
+/*
+ * An abstract pattern unit that holds modifiers and specifies an iterator pattern over matches.
+ * The return value of HasNextMatch, NextMatch and PrevMatch are undefined before Initialize has been called. NextMatch
+ * is only guaranteed to be specified if HasNextMatch returns true. PrevMatch is only specified if NextMatch has been
+ * called. The intended use is as follows:
+ *    unique_ptr<PatternUnit> pu = ...
+ *    string sequence = ...
+ *    pu->Initialize( sequence.cbegin() );
+ *    while( pu->HasNextMatch() ){
+ *      Match& m = pu->NextMatch();
+ *      // Do something with m
+ *    }
+ */
 class PatternUnit
 {
 public:
   PatternUnit(const Modifiers& modifiers);
 
   virtual void Initialize(std::string::const_iterator& pos) = 0;
-
   virtual bool HasNextMatch() = 0;
   virtual Match& NextMatch() = 0;
   virtual Match& PrevMatch() = 0;
