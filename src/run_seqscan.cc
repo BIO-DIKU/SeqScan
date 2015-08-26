48085818820c59d65703df9dde3f7b33a5306c06
Martin Asser Hansen <mail@maasha.dk>
2015-08-26 22:51:22 +0200

    Linting

--- src/run_seqscan.cc
+++ src/run_seqscan.cc
@@ -28,20 +28,19 @@
 
 using namespace std;
 
-int main()
-{
+int main() {
   // Example of setting up the seqscan pattern "AATCA/1,0,0 TTTTTTC"
   Modifiers top_modifiers = Modifiers::CreateStdModifiers();
-  unique_ptr<CompositeUnit> root_unit( new CompositeUnit(top_modifiers) );
+  unique_ptr<CompositeUnit> root_unit(new CompositeUnit(top_modifiers) );
 
-  Modifiers first_modifiers = Modifiers::CreateMIDModifiers(1,0,0);
-  unique_ptr<PatternUnit> first_unit( new BacktrackSequenceUnit(first_modifiers, "AATCA"));
+  Modifiers first_modifiers = Modifiers::CreateMIDModifiers(1, 0, 0);
+  unique_ptr<PatternUnit> first_unit(new BacktrackSequenceUnit(first_modifiers, "AATCA"));
 
   Modifiers second_modifiers = Modifiers::CreateStdModifiers();
-  unique_ptr<PatternUnit> second_unit( new BacktrackSequenceUnit(second_modifiers, "TTTTTTC"));
+  unique_ptr<PatternUnit> second_unit(new BacktrackSequenceUnit(second_modifiers, "TTTTTTC"));
 
-  Modifiers tnfa_modifiers = Modifiers::CreateMIDModifiers(1,0,0);
-  unique_ptr<PatternUnit> tnfa_unit( new TNFASequenceUnit(first_modifiers, "AATCA"));
+  Modifiers tnfa_modifiers = Modifiers::CreateMIDModifiers(1, 0, 0);
+  unique_ptr<PatternUnit> tnfa_unit(new TNFASequenceUnit(first_modifiers, "AATCA"));
 
   root_unit->AddUnit(first_unit);
   root_unit->AddUnit(second_unit);
@@ -51,25 +50,26 @@ int main()
   // Define sequence
   string sequence = "AUGAUGAUGAATTATTTTTTCGGG";
 
-  //Find all matches in sequence
+  // Find all matches in sequence
   int num_matches = 0;
-  root_unit->Initialize( sequence.cbegin(), sequence.cend() );
-  while(root_unit->HasNextMatch()){
+
+  root_unit->Initialize(sequence.cbegin(), sequence.cend());
+  while (root_unit->HasNextMatch()) {
     Match& m = root_unit->NextMatch();
-    // TODO: Do something with m
+    // TODO(whomever wrote this): Do something with m
     num_matches++;
   }
-  cout<<"Found "<<num_matches<<" matches"<<endl;
+  cout << "Found " << num_matches << " matches" << endl;
 
-  //Find matches using TNFA
+  // Find matches using TNFA
   num_matches = 0;
-  tnfa_unit->Initialize( sequence.cbegin(), sequence.cend() );
-  while(tnfa_unit->HasNextMatch()){
+  tnfa_unit->Initialize(sequence.cbegin(), sequence.cend());
+  while (tnfa_unit->HasNextMatch()) {
     Match& m = tnfa_unit->NextMatch();
-    printf( "Starting position: %i\tLength: %i\n", m.pos, m.len);
+    printf("Starting position: %i\tLength: %i\n", m.pos, m.len);
     num_matches++;
   }
-  cout<<"Found "<<num_matches<<" TNFA matches"<<endl;
+  cout << "Found " << num_matches << " TNFA matches" << endl;
 
-  return 0;
+  return EXIT_SUCCESS;
 }
