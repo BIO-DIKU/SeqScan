3f20adbb69cf1295df3bf49ce13aa25e68778caa
Sune Grud Enevoldsen <sune@antrit.net>
2015-08-27 22:23:51 +0200

    Add test case for simple fuzzy sequence

---
+++ src/tests/test_fuzzy_sequence.cc
@@ -0,0 +1,34 @@
+
+#include "catch.h"
+
+#include <memory>
+
+#include "../modifiers.h"
+#include "../pu/backtrack_sequence_unit.h"
+
+
+using namespace std;
+
+TEST_CASE( "Test sequence with fuzzy matching", "[sequence,fuzzy matching]" ) {
+
+  // Set up test pattern "AATCA/1,0,0"
+  Modifiers first_modifiers = Modifiers::CreateMIDModifiers(1, 0, 0);
+  unique_ptr<PatternUnit> first_unit(new BacktrackSequenceUnit(first_modifiers, "AATCA"));
+
+  // Define sequence
+  string sequence = "AUGAUGAUGAATTATTTTTTCGGG";
+  int num_matches = 0;
+
+  SECTION( "test match with 1 mismatch" ) {
+    first_unit->Initialize(sequence.cbegin(), sequence.cend());
+    while (first_unit->HasNextMatch()) {
+      const Match& m = first_unit->NextMatch();
+      REQUIRE( m.pos-sequence.cbegin() == 9 );
+      REQUIRE( m.len == 5 );
+      REQUIRE( m.edits == 1 );
+      num_matches++;
+    }	
+    
+    REQUIRE( num_matches == 1 );
+  }
+}
