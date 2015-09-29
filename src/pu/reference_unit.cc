f6cd913ab13e9c7cc7e9ad349c9a4c91894b36dc
Martin Asser Hansen <mail@maasha.dk>
2015-09-29 14:25:06 +0200

    Linting

--- src/pu/reference_unit.cc
+++ src/pu/reference_unit.cc
@@ -22,50 +22,43 @@
 #include "reference_unit.h"
 #include "backtrack_unit.h"
 
-ReferenceUnit::ReferenceUnit( PatternUnit* pu, const Modifiers &modifiers ):
+ReferenceUnit::ReferenceUnit(PatternUnit* pu, const Modifiers &modifiers):
     PatternUnit(modifiers),
     referenced_unit_(pu)
-{
-}
+{}
 
 void ReferenceUnit::Initialize(
     std::string::const_iterator pos,
     std::string::const_iterator max_pos,
     bool stay_at_pos
-)
-{
+) {
   const Match& match = referenced_unit_->GetMatch();
   std::string pattern(match.pos, match.pos+match.len);
-  pattern_finder_ = std::unique_ptr<PatternUnit>( new BacktrackUnit(modifiers_, pattern) );
+  pattern_finder_ = std::unique_ptr<PatternUnit>(new BacktrackUnit(modifiers_, pattern));
   pattern_finder_->Initialize(pos, max_pos, stay_at_pos);
 }
 
-bool ReferenceUnit::FindMatch()
-{
+bool ReferenceUnit::FindMatch() {
   return pattern_finder_->FindMatch();
 }
 
-const Match& ReferenceUnit::GetMatch() const
-{
+const Match& ReferenceUnit::GetMatch() const {
   return pattern_finder_->GetMatch();
 }
 
-std::ostream& ReferenceUnit::Print(std::ostream &os) const
-{
+std::ostream& ReferenceUnit::Print(std::ostream &os) const {
   modifiers_.PrintPUPrefix(os);
 
-  os<<referenced_unit_->modifiers_.label_;
+  os << referenced_unit_->modifiers_.label_;
 
   modifiers_.PrintPUSuffix(os);
   return os;
 }
 
-std::unique_ptr<PatternUnit> ReferenceUnit::Clone() const
-{
+std::unique_ptr<PatternUnit> ReferenceUnit::Clone() const {
   std::unique_ptr<PatternUnit> ret =
       std::unique_ptr<PatternUnit>(
-          new ReferenceUnit(referenced_unit_,modifiers_)
-      );
+          new ReferenceUnit(referenced_unit_, modifiers_));
 
   return ret;
 }
