7f7c4b1a4c616ad3d4869d9d93cc8eeb053e1c7e
Martin Asser Hansen <mail@maasha.dk>
2015-09-29 13:49:17 +0200

    Linting

--- src/io.cc
+++ src/io.cc
@@ -25,6 +25,7 @@
 #include <fstream>
 
 using namespace std;
+
 /**
  * @brief read_file reads a fasta-formatted file.
  * All characters are converted to upper-case.
@@ -32,14 +33,14 @@ using namespace std;
  * @return a list of pairs representing each entry in the fasta file. The first member is the
  * header line and the second entry is the full sequence.
  */
-vector<pair<string,string>> IO::read_fasta(const string& fname){
+vector<pair<string, string>> IO::read_fasta(const string& fname) {
   ifstream in(fname.c_str(), std::ios::in | std::ios::binary);
-  vector<pair<string,string>> ret;
-  pair<string, string> record("","");
+  vector<pair<string, string>> ret;
+  pair<string, string> record("", "");
 
-  //This is more efficient, but doesnt check for headers
-  //From http://insanecoding.blogspot.dk/2011/11/how-to-read-in-file-in-c.html
-  //if (in){
+  // This is more efficient, but doesnt check for headers
+  // From http://insanecoding.blogspot.dk/2011/11/how-to-read-in-file-in-c.html
+  // if (in){
   //    //std::string contents;
   //    in.seekg(0, std::ios::end);
   //    record.second.resize(in.tellg());
@@ -51,17 +52,16 @@ vector<pair<string,string>> IO::read_fasta(const string& fname){
   //    ret.push_back(std::move(record));
   //    return std::move(ret);
   //    //return std::move(contents);
-  //}
-  //throw(errno);
-
+  // }
+  // throw(errno);
 
   string line;
-  while (!in.eof()){
-    getline(in,line);
+  while (!in.eof()) {
+    getline(in, line);
 
-    if (line[0] == '>'){
-      if(!record.first.empty())
-        ret.push_back( std::move(record) );
+    if (line[0] == '>') {
+      if (!record.first.empty())
+        ret.push_back(std::move(record));
 
       record = std::make_pair(line, "");
       continue;
@@ -71,8 +71,6 @@ vector<pair<string,string>> IO::read_fasta(const string& fname){
     record.second.append(line);
   }
   in.close();
-  ret.push_back( std::move(record) );
+  ret.push_back(std::move(record));
   return std::move(ret);
-
 }
-
