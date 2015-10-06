2030d84d1360be4e3718c776a2e27c2fd6066ae8
Martin Asser Hansen <mail@maasha.dk>
2015-10-06 21:55:49 +0200

    Linting

--- tests/pu/test_backtrack_unit.cc
+++ tests/pu/test_backtrack_unit.cc
@@ -18,106 +18,103 @@
  * http://www.gnu.org/copyleft/gpl.html
  */
 
-#include "../catch.h"
-
 #include <memory>
 #include <iostream>
 
+#include "../catch.h"
 #include "../../src/modifiers.h"
 #include "../../src/match.h"
 #include "../../src/pu/backtrack_unit.h"
 
-
 using namespace std;
 
-TEST_CASE( "Test backtrack sequence unit matching without fuzziness", "[backtrack,exact]" ) {
-
+TEST_CASE("Test backtrack sequence unit matching without fuzziness", "[backtrack,exact]") {
   // Set up test pattern "AAAA/0,0,0"
   Modifiers modifiers = Modifiers::CreateMIDModifiers(0, 0, 0);
   unique_ptr<PatternUnit> pu(new BacktrackUnit(modifiers, "AAAA"));
 
-  SECTION( "0 matches" ){
+  SECTION("0 matches") {
     string sequence = "TTTAAATTT";
     pu->Initialize(sequence.cbegin(), sequence.cend());
-    REQUIRE( !pu->FindMatch() );
-    REQUIRE( !pu->FindMatch() );
+    REQUIRE(!pu->FindMatch());
+    REQUIRE(!pu->FindMatch());
 
     sequence = "AAATTT";
     pu->Initialize(sequence.cbegin(), sequence.cend());
-    REQUIRE( !pu->FindMatch() );
-    REQUIRE( !pu->FindMatch() );
+    REQUIRE(!pu->FindMatch());
+    REQUIRE(!pu->FindMatch());
 
     sequence = "TTTAAA";
     pu->Initialize(sequence.cbegin(), sequence.cend());
-    REQUIRE( !pu->FindMatch() );
-    REQUIRE( !pu->FindMatch() );
+    REQUIRE(!pu->FindMatch());
+    REQUIRE(!pu->FindMatch());
   }
 
-  SECTION( "1 match" ){
+  SECTION("1 match") {
     string sequence = "TTTAAAATTT";
     pu->Initialize(sequence.cbegin(), sequence.cend());
     REQUIRE(pu->FindMatch() );
 
     const Match& m1 = pu->GetMatch();
-    REQUIRE( m1.pos-sequence.cbegin() == 3 );
-    REQUIRE( m1.len == 4 );
-    REQUIRE( m1.edits == 0 );
+    REQUIRE(m1.pos - sequence.cbegin() == 3);
+    REQUIRE(m1.len == 4);
+    REQUIRE(m1.edits == 0);
 
-    REQUIRE( !pu->FindMatch() );
+    REQUIRE(!pu->FindMatch());
 
     sequence = "AAAATTT";
     pu->Initialize(sequence.cbegin(), sequence.cend());
     REQUIRE(pu->FindMatch() );
 
     const Match& m2 = pu->GetMatch();
-    REQUIRE( m2.pos-sequence.cbegin() == 0 );
-    REQUIRE( m2.len == 4 );
-    REQUIRE( m2.edits == 0 );
+    REQUIRE(m2.pos - sequence.cbegin() == 0);
+    REQUIRE(m2.len == 4);
+    REQUIRE(m2.edits == 0);
 
-    REQUIRE( !pu->FindMatch() );
+    REQUIRE(!pu->FindMatch());
 
     sequence = "TTTAAAA";
     pu->Initialize(sequence.cbegin(), sequence.cend());
-    REQUIRE(pu->FindMatch() );
-    REQUIRE( !pu->FindMatch() );
+    REQUIRE(pu->FindMatch());
+    REQUIRE(!pu->FindMatch());
   }
 
-  SECTION( "2 non-overlapping matches" ){
+  SECTION("2 non-overlapping matches") {
     string sequence = "TAAAATAAAAT";
     pu->Initialize(sequence.cbegin(), sequence.cend());
     REQUIRE(pu->FindMatch());
 
     const Match& m1 = pu->GetMatch();
-    REQUIRE( m1.pos-sequence.cbegin() == 1 );
-    REQUIRE( m1.len == 4 );
-    REQUIRE( m1.edits == 0 );
+    REQUIRE(m1.pos - sequence.cbegin() == 1);
+    REQUIRE(m1.len == 4);
+    REQUIRE(m1.edits == 0);
 
     REQUIRE(pu->FindMatch());
 
     const Match& m2 = pu->GetMatch();
-    REQUIRE( m2.pos-sequence.cbegin() == 6 );
-    REQUIRE( m2.len == 4 );
-    REQUIRE( m2.edits == 0 );
+    REQUIRE(m2.pos - sequence.cbegin() == 6);
+    REQUIRE(m2.len == 4);
+    REQUIRE(m2.edits == 0);
 
     REQUIRE(!pu->FindMatch());
   }
 
-  SECTION( "2 overlapping matches" ){
+  SECTION("2 overlapping matches") {
     string sequence = "TTTAAAAAT";
     pu->Initialize(sequence.cbegin(), sequence.cend());
     REQUIRE(pu->FindMatch());
 
     const Match& m1 = pu->GetMatch();
-    REQUIRE( m1.pos-sequence.cbegin() == 3 );
-    REQUIRE( m1.len == 4 );
-    REQUIRE( m1.edits == 0 );
+    REQUIRE(m1.pos - sequence.cbegin() == 3);
+    REQUIRE(m1.len == 4);
+    REQUIRE(m1.edits == 0);
 
     REQUIRE(pu->FindMatch());
 
     const Match& m2 = pu->GetMatch();
-    REQUIRE( m2.pos-sequence.cbegin() == 4 );
-    REQUIRE( m2.len == 4 );
-    REQUIRE( m2.edits == 0 );
+    REQUIRE(m2.pos - sequence.cbegin() == 4);
+    REQUIRE(m2.len == 4);
+    REQUIRE(m2.edits == 0);
 
     REQUIRE(!pu->FindMatch());
 
@@ -129,9 +126,7 @@ TEST_CASE( "Test backtrack sequence unit matching without fuzziness", "[backtrac
   }
 }
 
-
-TEST_CASE( "Test backtrack sequence unit matching with mismatches", "[backtrack,mismatches]" ) {
-
+TEST_CASE("Test backtrack sequence unit matching with mismatches", "[backtrack,mismatches]") {
   // Set up test pattern "AAAA/1,0,0"
   Modifiers modifiers = Modifiers::CreateMIDModifiers(1, 0, 0);
   unique_ptr<PatternUnit> pu(new BacktrackUnit(modifiers, "AAAA"));
@@ -140,82 +135,81 @@ TEST_CASE( "Test backtrack sequence unit matching with mismatches", "[backtrack,
   Modifiers modifiers_2 = Modifiers::CreateMIDModifiers(2, 0, 0);
   unique_ptr<PatternUnit> pu_2(new BacktrackUnit(modifiers, "AAAA"));
 
-  SECTION( "0 matches" ){
+  SECTION("0 matches") {
     string sequence = "TTTAATTT";
     pu->Initialize(sequence.cbegin(), sequence.cend());
-    REQUIRE( !pu->FindMatch() );
-    REQUIRE( !pu->FindMatch() );
+    REQUIRE(!pu->FindMatch());
+    REQUIRE(!pu->FindMatch());
 
     sequence = "AATTT";
     pu->Initialize(sequence.cbegin(), sequence.cend());
-    REQUIRE( !pu->FindMatch() );
-    REQUIRE( !pu->FindMatch() );
+    REQUIRE(!pu->FindMatch());
+    REQUIRE(!pu->FindMatch());
 
     sequence = "TTTAA";
     pu->Initialize(sequence.cbegin(), sequence.cend());
-    REQUIRE( !pu->FindMatch() );
-    REQUIRE( !pu->FindMatch() );
+    REQUIRE(!pu->FindMatch());
+    REQUIRE(!pu->FindMatch());
   }
 
-  SECTION( "1 match" ){
+  SECTION("1 match") {
     string sequence = "TTTAACATTT";
     pu->Initialize(sequence.cbegin(), sequence.cend());
-    REQUIRE(pu->FindMatch() );
+    REQUIRE(pu->FindMatch());
 
     const Match& m1 = pu->GetMatch();
-    REQUIRE( m1.pos-sequence.cbegin() == 3 );
-    REQUIRE( m1.len == 4 );
-    REQUIRE( m1.edits == 1 );
+    REQUIRE(m1.pos - sequence.cbegin() == 3);
+    REQUIRE(m1.len == 4);
+    REQUIRE(m1.edits == 1);
 
-    REQUIRE( !pu->FindMatch() );
+    REQUIRE(!pu->FindMatch());
 
     sequence = "AAAGTTT";
     pu->Initialize(sequence.cbegin(), sequence.cend());
-    REQUIRE(pu->FindMatch() );
+    REQUIRE(pu->FindMatch());
 
     const Match& m2 = pu->GetMatch();
-    REQUIRE( m2.pos-sequence.cbegin() == 0 );
-    REQUIRE( m2.len == 4 );
-    REQUIRE( m2.edits == 1 );
+    REQUIRE(m2.pos - sequence.cbegin() == 0);
+    REQUIRE(m2.len == 4);
+    REQUIRE(m2.edits == 1);
 
-    REQUIRE( !pu->FindMatch() );
+    REQUIRE(!pu->FindMatch());
 
     sequence = "CCCUAAA";
     pu->Initialize(sequence.cbegin(), sequence.cend());
-    REQUIRE(pu->FindMatch() );
-    REQUIRE( !pu->FindMatch() );
+    REQUIRE(pu->FindMatch());
+    REQUIRE(!pu->FindMatch());
   }
 
-  SECTION( "overlapping matches" ){
+  SECTION("overlapping matches") {
     string sequence = "TTTAAACAT";
     pu->Initialize(sequence.cbegin(), sequence.cend());
     REQUIRE(pu->FindMatch());
 
     const Match& m1 = pu->GetMatch();
-    REQUIRE( m1.pos-sequence.cbegin() == 2 );
-    REQUIRE( m1.len == 4 );
-    REQUIRE( m1.edits == 1 );
+    REQUIRE(m1.pos - sequence.cbegin() == 2);
+    REQUIRE(m1.len == 4);
+    REQUIRE(m1.edits == 1);
 
     REQUIRE(pu->FindMatch());
 
     const Match& m2 = pu->GetMatch();
-    REQUIRE( m2.pos-sequence.cbegin() == 3 );
-    REQUIRE( m2.len == 4 );
-    REQUIRE( m2.edits == 1 );
+    REQUIRE(m2.pos - sequence.cbegin() == 3);
+    REQUIRE(m2.len == 4);
+    REQUIRE(m2.edits == 1);
 
     REQUIRE(pu->FindMatch());
 
     const Match& m3 = pu->GetMatch();
-    REQUIRE( m3.pos-sequence.cbegin() == 4 );
-    REQUIRE( m3.len == 4 );
-    REQUIRE( m3.edits == 1 );
+    REQUIRE(m3.pos - sequence.cbegin() == 4);
+    REQUIRE(m3.len == 4);
+    REQUIRE(m3.edits == 1);
 
     REQUIRE(!pu->FindMatch());
   }
 }
 
-TEST_CASE( "Backtrack unit staying at pos", "[backtrack,stay]" ) {
-
+TEST_CASE("Backtrack unit staying at pos", "[backtrack,stay]") {
   // Set up test pattern "AAAA/1,0,0"
   Modifiers modifiers = Modifiers::CreateMIDModifiers(1, 0, 0);
   unique_ptr<PatternUnit> pu(new BacktrackUnit(modifiers, "AAAA"));
@@ -252,13 +246,9 @@ TEST_CASE( "Backtrack unit staying at pos", "[backtrack,stay]" ) {
 
     REQUIRE(!pu->FindMatch());
   }
-
 }
 
-
-TEST_CASE( "Backtrack unit staying at pos; several matches", "[backtrack,stay]")
-{
-
+TEST_CASE("Backtrack unit staying at pos; several matches", "[backtrack,stay]") {
   // Set up test pattern "AAAA/1,0,0"
   Modifiers modifiers = Modifiers::CreateMIDModifiers(0, 1, 0);
   unique_ptr<PatternUnit> pu(new BacktrackUnit(modifiers, "AAAA"));
