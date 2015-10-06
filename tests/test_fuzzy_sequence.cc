260924135541e192496bba45ad4255080624b1f6
Martin Asser Hansen <mail@maasha.dk>
2015-10-06 21:41:43 +0200

    Linting

--- tests/test_fuzzy_sequence.cc
+++ tests/test_fuzzy_sequence.cc
@@ -18,16 +18,15 @@
  * http://www.gnu.org/copyleft/gpl.html
  */
 
-#include "catch.h"
-
 #include <memory>
 
+#include "catch.h"
 #include "../src/modifiers.h"
 #include "../src/pu/backtrack_unit.h"
 
 using namespace std;
 
-TEST_CASE( "Test sequence with fuzzy matching", "[sequence,fuzzy matching]" ) {
+TEST_CASE("Test sequence with fuzzy matching", "[sequence,fuzzy matching]") {
 
   // Set up test pattern "AATCA/1,0,0"
   Modifiers first_modifiers = Modifiers::CreateMIDModifiers(1, 0, 0);
@@ -37,16 +36,16 @@ TEST_CASE( "Test sequence with fuzzy matching", "[sequence,fuzzy matching]" ) {
   string sequence = "AUGAUGAUGAATTATTTTTTCGGG";
   int num_matches = 0;
 
-  SECTION( "test match with 1 mismatch" ) {
+  SECTION("test match with 1 mismatch") {
     first_unit->Initialize(sequence.cbegin(), sequence.cend());
     while (first_unit->FindMatch()) {
       const Match& m = first_unit->GetMatch();
-      REQUIRE( m.pos-sequence.cbegin() == 9 );
-      REQUIRE( m.len == 5 );
-      REQUIRE( m.edits == 1 );
+      REQUIRE(m.pos-sequence.cbegin() == 9);
+      REQUIRE(m.len == 5);
+      REQUIRE(m.edits == 1);
       num_matches++;
     }
 
-    REQUIRE( num_matches == 1 );
+    REQUIRE(num_matches == 1);
   }
 }
