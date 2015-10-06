c42f4264627a9582c419713372c45241659886cc
Martin Asser Hansen <mail@maasha.dk>
2015-10-06 20:36:01 +0200

    removed redundant del.sh and run.sh - use Makefile instead

--- parser/del.sh
+++
@@ -1,1 +0,0 @@
-rm lex.yy.c seq_par.tab.c seq_par.tab.h seqscan y.tab.c y.tab.h seq_par.output
--- parser/run.sh
+++
@@ -1,3 +0,0 @@
-bison -d seq_par.y
-flex seq_lex.l
-g++ -std=c++11 parse_tree_list.cpp parse_tree_unit.cpp seq_par.tab.c lex.yy.c -lfl -o seqscan
