aa29dd0d31b30a6a925f1f71f6675509b08bf496
Martin Asser Hansen <mail@maasha.dk>
2015-10-19 15:09:04 +0200

    Created OptParse Class

--- src/optparse.h
+++ src/optparse.h
@@ -21,41 +21,106 @@
 #ifndef SEQSCAN_OPTPARSE_H_
 #define SEQSCAN_OPTPARSE_H_
 
-/*
- * Optios stucture for holding what-the-hell-is-it?
- * TODO(Martin) find the proper getopt web page and figure out what is going on.
- */
-static struct options long_options[] = {
-  {"help",           no_argument,       0, 'h'},
-  {"pattern",        required_argument, 0, 'p'},
-  {"pattern_file",   required_argument, 0, 'P'},
-  {"complement",     required_argument, 0, 'c'},
-  {"direction",      required_argument, 0, 'd'},
-  {"start",          required_argument, 0, 's'},
-  {"end",            required_argument, 0, 'e'},
-  {"threads",        required_argument, 0, 't'},
-  {"score_encoding", required_argument, 0, 'E'},
-  {"score_min",      required_argument, 0, 'S'},
-  {"ambiguate",      no_argument,       0, 'a'},
-  {"match_type",     required_argument, 0, 'm'},
-  {"match_file",     required_argument, 0, 'M'},
-  {"output",         required_argument, 0, 'o'},
-  {"overlap",        no_argument,       0, 'O'},
-  {"filter" ,        required_argument, 0, 'f'},
-  {"version",        no_argument,       0, 'v'},
-  {"verbose",        no_argument,       0, 'V'},
-  {0,                 0,                0,  0 }
-};
+#include <string>
+#include <map>
 
-/*
- * Parse options from argv and save the result in long_options.
- * Returns true if parsing was OK else false.
+/**
+ * @brief Class for parsing command line arguments using Getopt-Long:
+ *
+ * http://www.gnu.org/software/libc/manual/html_node/Getopt-Long-Option-Example.html
  */
-static bool OptParse(int argc, char *argv[], struct options long_options);
+class OptParse {
+ public:
+  OptParse(int argc, char *argv[]);
 
-/*
- * Print usage to stderr.
- */
-static void PrintUsage();
+  ~OptParse();
+
+  /*
+   * Parse options from argv and save the result in Options struct.
+   * Returns true if parsing was OK else false.
+   */
+  bool Parse();
+
+  /*
+   * Print usage to stderr.
+   */
+  void PrintUsage();
+
+ private:
+
+  /*
+   * Struct for holding option template.
+   */
+  struct OptTemplate {
+    {"help",           no_argument,       0, 'h'},
+    {"pattern",        required_argument, 0, 'p'},
+    {"pattern_file",   required_argument, 0, 'P'},
+    {"complement",     required_argument, 0, 'c'},
+    {"direction",      required_argument, 0, 'd'},
+    {"start",          required_argument, 0, 's'},
+    {"end",            required_argument, 0, 'e'},
+    {"threads",        required_argument, 0, 't'},
+    {"score_encoding", required_argument, 0, 'E'},
+    {"score_min",      required_argument, 0, 'S'},
+    {"ambiguate",      no_argument,       0, 'a'},
+    {"match_type",     required_argument, 0, 'm'},
+    {"match_file",     required_argument, 0, 'M'},
+    {"output",         required_argument, 0, 'o'},
+    {"overlap",        no_argument,       0, 'O'},
+    {"filter" ,        required_argument, 0, 'f'},
+    {"version",        no_argument,       0, 'v'},
+    {"verbose",        no_argument,       0, 'V'},
+    {0,                 0,                0,  0 }
+  };
+
+  /*
+   * Enum for direction option.
+   */
+  enum Direction {
+    Forward,
+    Reverse
+  };
+
+  /*
+   * Enum for complement option.
+   */
+  enum Complement {
+    Forward,
+    Reverse,
+    Both
+  };
+
+  /*
+   * Enum for score encoding option.
+   */
+  enum ScoreEncoding {
+    Phred33,
+    Phred64
+  };
+
+  /*
+   * Struct for parsed command line options.
+   */
+  struct Options {
+    bool        help;
+    std::string pattern;
+    std::string pattern_file;
+    enum        Complement;
+    enum        Direction;
+    int         start;
+    int         end;
+    int         threads;
+    enum        ScoreEncoding;
+    int         score_min;
+    bool        ambiguate;
+    int         match_type;
+    std::string match_file;
+    std::string output;
+    bool        overlap;
+    std::string filter;
+    bool        version;
+    bool        verbose;
+  };
+}
 
 #endif  // SEQSCAN_OPTPARSE_H_
