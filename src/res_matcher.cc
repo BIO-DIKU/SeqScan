658187dde14b910a265dfa2a248f02fd63ebf3be
Martin Asser Hansen <mail@maasha.dk>
2015-11-11 11:43:12 +0100

    Linting

--- src/res_matcher.cc
+++ src/res_matcher.cc
@@ -22,6 +22,7 @@
 #include <iostream>
 #include <sstream>
 #include <bitset>
+#include <vector>
 
 #include "res_matcher.h"
 #include "matrix_io.h"
@@ -56,7 +57,7 @@ void ResMatcher::set(size_t index) {
   res_matcher_.set(index, true);
 }
 
-bool ResMatcher::is_set(size_t index) const{
+bool ResMatcher::is_set(size_t index) const {
   return res_matcher_.test(index);
 }
 
