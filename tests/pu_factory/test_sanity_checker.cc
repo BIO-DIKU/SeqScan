8cb3597086f88f1f5fb597fea652e7270ba882ac
Martin Asser Hansen <mail@maasha.dk>
2015-11-16 20:14:13 +0100

    added test files

---
+++ tests/pu_factory/test_sanity_checker.cc
@@ -0,0 +1,32 @@
+/*
+ * Copyright (C) 2015 BIO-DIKU.
+ *
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the Free
+ * Software Foundation; either version 2 of the License, or (at your option)
+ * any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
+ * more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc., 51
+ * Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA
+ *
+ * http://www.gnu.org/copyleft/gpl.html
+ */
+
+#include <string>
+
+#include "catch.h"
+#include "../src/pu_factory/pattern_unit_creator.h"
+
+using namespace std;
+
+TEST_CASE("PatternUnitCreator creates PUs", "[pattern_unit_creator]") {
+  SECTION("foo") {
+    REQUIRE(true);
+  }
+}
