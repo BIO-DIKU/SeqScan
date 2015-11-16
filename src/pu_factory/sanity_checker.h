accedee263d0be143591d5ed7883e1f7a3d886d6
Martin Asser Hansen <mail@maasha.dk>
2015-11-16 19:46:46 +0100

    Linting

--- src/pu_factory/sanity_checker.h
+++ src/pu_factory/sanity_checker.h
@@ -1,9 +1,25 @@
-//
-// Created by Rasmus Fonseca on 25/10/15.
-//
+/*
+ * Copyright (C) 2015 BIO-DIKU.
+ *
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the Free
+ * Software Foundation; either version 2 of the License, or (at your option)
+ * any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
+ * more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc., 51
+ * Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA
+ *
+ * http://www.gnu.org/copyleft/gpl.html
+ */
 
-#ifndef SEQSCAN_SANITY_CHECKER_H
-#define SEQSCAN_SANITY_CHECKER_H
+#ifndef SEQSCAN_PU_FACTORY_SANITY_CHECKER_H_
+#define SEQSCAN_PU_FACTORY_SANITY_CHECKER_H_
 
 #include <set>
 #include <string>
@@ -11,27 +27,25 @@
 #include <parser/parse_tree_unit.h>
 
 namespace SeqScan {
+class SanityChecker {
+ public:
 
-  class SanityChecker {
+  /**
+   * Performs sanity checks on the specified parse tree. Prints errors to
+   * cerr and returns false if anything appears that will make the search
+   * with PatternUnit break down.
+   */
+  bool is_sane(const ParseTreeUnit *parse_tree);
 
-   public:
+ private:
+  bool is_sane(const ParseTreeUnit *parse_tree, std::set<std::string>& visited_labels);
 
-    /**
-     * Performs sanity checks on the specified parse tree. Prints errors to cerr and returns false if anything appears
-     * that will make the search with PatternUnit break down.
-     */
-    bool is_sane(const ParseTreeUnit *parse_tree);
+  bool check_reference(const ParseTreeUnit *node, std::set<std::string>& visited_labels);
+  bool check_sequence(const ParseTreeUnit *node);
+  bool check_composite(const ParseTreeUnit *node, std::set<std::string>& visited_labels);
+  bool check_repeat(const ParseTreeUnit *node, std::set<std::string>& visited_labels);
 
-   private:
-    bool is_sane(const ParseTreeUnit *parse_tree, std::set<std::string>& visited_labels);
-
-    bool check_reference(const ParseTreeUnit *node, std::set<std::string>& visited_labels);
-    bool check_sequence(const ParseTreeUnit *node);
-    bool check_composite(const ParseTreeUnit *node, std::set<std::string>& visited_labels);
-    bool check_repeat(const ParseTreeUnit *node, std::set<std::string>& visited_labels);
-
-  };
-
+};
 } // namespace SeqScan
 
-#endif //SEQSCAN_SANITY_CHECKER_H
+#endif  // SEQSCAN_PU_FACTORY_SANITY_CHECKER_H_
